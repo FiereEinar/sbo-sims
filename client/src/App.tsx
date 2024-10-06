2f3f02290b01a6acd65150937e210e061ba8bec2
Nick Mours <nickxylanmelloria@gmail.com>
2024-10-06 08:46:52 +0800

    setup client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button';
+
 function App() {
 	return (
-		<main>
-			<h1>Hello World!</h1>
+		<main className='dark p-5 bg-background'>
+			<div className='bg-card p-3'>
+				<h1 className='text-red-500'>Hello World!</h1>
+				<Button size='sm'>Click Me!</Button>
+			</div>
 		</main>
 	);
 }

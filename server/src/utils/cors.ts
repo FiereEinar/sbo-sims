fd9aeb1afc5a9029d5c8f19852b500c67a69429e
Nick Mours <nickxylanmelloria@gmail.com>
2025-01-25 13:49:03 +0800

    updated cors

--- server/src/utils/cors.ts
+++ server/src/utils/cors.ts
@@ -1,20 +1,20 @@
 import { CorsOptions } from 'cors';
 import { FRONTEND_URL, IPV4_ADDRESS_ORIGIN } from '../constants/env';
 
-const allowedOrigins = [FRONTEND_URL, IPV4_ADDRESS_ORIGIN];
-
-// export const corsOptions: CorsOptions = {
-// 	origin: FRONTEND_URL,
-// 	credentials: true,
-// };
+// const allowedOrigins = [FRONTEND_URL, IPV4_ADDRESS_ORIGIN];
 
 export const corsOptions: CorsOptions = {
-	origin: (origin, callback) => {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
+	origin: FRONTEND_URL,
 	credentials: true,
 };
+
+// export const corsOptions: CorsOptions = {
+// 	origin: (origin, callback) => {
+// 		if (!origin || allowedOrigins.includes(origin)) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error('Not allowed by CORS'));
+// 		}
+// 	},
+// 	credentials: true,
+// };

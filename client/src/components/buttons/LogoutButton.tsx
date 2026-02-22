e98181ea2465829b03a02808cac6f5d75c4e6c41
Nick Mours <nickxylanmelloria@gmail.com>
2026-02-22 08:03:36 +0800

    added dialog for logout

--- client/src/components/buttons/LogoutButton.tsx
+++ client/src/components/buttons/LogoutButton.tsx
@@ -3,6 +3,17 @@ import SidebarLink from '../SidebarLink';
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '@/hooks/use-toast';
 import { Power } from 'lucide-react';
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '../ui/alert-dialog';
 
 export default function LogoutButton() {
 	const { toast } = useToast();
@@ -24,10 +35,28 @@ export default function LogoutButton() {
 	};
 
 	return (
-		<button onClick={onLogout}>
-			<div className='pointer-events-none'>
-				<SidebarLink link={{ icon: Power, name: 'Logout', path: '/logout' }} />
-			</div>
-		</button>
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<button>
+					<div className='pointer-events-none'>
+						<SidebarLink
+							link={{ icon: Power, name: 'Logout', path: '/logout' }}
+						/>
+					</div>
+				</button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you sure you want to logout?</AlertDialogTitle>
+					<AlertDialogDescription>
+						You will need to login again to access your account.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction onClick={onLogout}>Logout</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	);
 }

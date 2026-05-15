68d3cdeeade5ec6e99628299b59ed7b2dd0c88b8
FiereEinar <nickxylanmelloria@gmail.com>
2026-05-15 08:36:32 +0800

    updated top bar

--- client/src/components/TopNavbar.tsx
+++ client/src/components/TopNavbar.tsx
@@ -6,7 +6,7 @@ export default function TopNavbar() {
 	const { user } = useUserStore((state) => state);
 
 	return (
-		<div className='w-full sticky top-0 z-50 bg-card/50 p-2 px-4 border-b flex justify-between items-center h-16'>
+		<div className='w-full sticky top-0 z-50 bg-card/50 p-2 px-4 border-b flex flex-row-reverse sm:flex-row justify-between items-center h-16'>
 			<div className='flex items-center gap-3'>
 				<div className='sm:hidden'>
 					<TopNavbarSheet />

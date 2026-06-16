5e80c74e2cf387388bfd0a932fd3aee98a19d698
FiereEinar <nickxylanmelloria@gmail.com>
2026-06-16 09:10:02 +0800

    removed delete button in organization

--- client/src/components/buttons/EditAndDeleteOrganizationButton.tsx
+++ client/src/components/buttons/EditAndDeleteOrganizationButton.tsx
@@ -1,22 +1,5 @@
-import { useTenantNavigate } from '../../hooks/useTenantNavigate';
-import { Trash2 } from 'lucide-react';
-import { Button } from '../ui/button';
-import { useToast } from '@/hooks/use-toast';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { queryClient } from '@/main';
-import { MODULES, QUERY_KEYS } from '@/constants';
+import { MODULES } from '@/constants';
 import AddOrganizationForm from '../forms/AddOrganizationForm';
-import { requestDeleteOrganization } from '@/api/organization';
 import HasPermission from '../HasPermission';
 
 type EditAndDeleteOrganizationButtonProps = {
@@ -26,67 +9,13 @@ type EditAndDeleteOrganizationButtonProps = {
 export default function EditAndDeleteOrganizationButton({
   organizationID,
 }: EditAndDeleteOrganizationButtonProps) {
-  return (
-    <div className="space-x-2 flex">
-      <HasPermission permissions={[MODULES.ORGANIZATION_UPDATE]}>
-        <EditButton organizationID={organizationID} />
-      </HasPermission>
-      <HasPermission permissions={[MODULES.ORGANIZATION_DELETE]}>
-        <DeleteButton organizationID={organizationID} />
-      </HasPermission>
-    </div>
-  );
-}
-
-type DeleteButtonProps = {
-  organizationID: string;
-};
-
-function DeleteButton({ organizationID }: DeleteButtonProps) {
-  const { toast } = useToast();
-  const navigate = useTenantNavigate();
-
-  const onDelete = async () => {
-    try {
-      navigate('/organization');
-
-      await requestDeleteOrganization(organizationID);
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.ORGANIZATION] });
-    } catch (err: any) {
-      console.error('Failed to delete organization', err);
-      toast({
-        variant: 'destructive',
-        title: 'Failed to delete organization',
-        description:
-          err.message ||
-          'A network error occured while trying to delete organization',
-      });
-    }
-  };
-
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button className="flex gap-1" variant="destructive" size="sm">
-          <Trash2 className="size-4" />
-          <p>Delete</p>
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete the
-            organization and remove the data from the database.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={onDelete}>Delete</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
+	return (
+		<div className="flex">
+			<HasPermission permissions={[MODULES.ORGANIZATION_UPDATE]}>
+				<EditButton organizationID={organizationID} />
+			</HasPermission>
+		</div>
+	);
 }
 
 function EditButton({ organizationID }: EditAndDeleteOrganizationButtonProps) {
